2fe638d487f5dbf336fff4340e8965dabf823b16
xlokox <knafel10@gmail.com>
2024-03-31 17:45:04 +0300

    big update on hamburger nav bar

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const navList = document.querySelector('.nav-list');
-
-    hamburgerMenu.addEventListener('click', function() {
-        navList.classList.toggle('show');
-    });
-});
